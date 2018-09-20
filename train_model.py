7c16c9d01dc6cc75cfd90cca11dc814df408c54d
clcarver1130 <carver.cameron@gmail.com>
2018-09-20 15:02:22 -0500

    changed activation parameter

--- train_model.py
+++ train_model.py
@@ -32,20 +32,16 @@ def train_model(img_list, label_list):
     # Create and compile model
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=(200, 200, 1)))
-    model.add(activation='relu')
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(32, (3, 3)))
-    model.add(activation='relu')
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(64, (3, 3)))
-    model.add(activation='relu')
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
     model.add(Dense(64))
-    model.add(activation='relu')
     model.add(Dropout(0.5))
     model.add(Dense(5, activation='softmax'))
 
