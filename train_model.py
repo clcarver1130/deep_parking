0b36f4f53ce9a4a33b757c1df37906475a35da55
clcarver1130 <carver.cameron@gmail.com>
2018-09-20 12:48:19 -0500

    training function implemented

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import pickle
 import keras
 from keras.preprocessing.image import img_to_array, load_img
 from keras.utils import to_categorical
@@ -10,41 +11,45 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 import matplotlib.pyplot as plt
 
-def import_images():
-    filelist = os.listdir('training_images/')
+print('Importing images...')
+def import_images(directory):
+    filelist = os.listdir(directory)
     train_list = []
     for file in filelist:
-        train_list.append(img_to_array(load_img('training_images/{}'.format(file), target_size=(48,48))))
+        train_list.append(img_to_array(load_img(directory'/{}'.format(file), target_size=(48,48))).reshape(-1, 48,48, 1)/255)
     return train_list
 
-def import_labels():
-    df = pd.read_csv('training_labels.csv')
+print('Importing labels...')
+def import_labels(CSV):
+    df = pd.read_csv(CSV)
     labels = df['Label']
     labels_cat = to_categorical(labels)
     return labels_cat
 
-def scale_images():
-    train_imgs_scaled = []
-    for img in train_list:
-        train_imgs_scaled.append(img/255)
-    return train_imgs_scaled
+print('Fitting model...')
+def train_model(img_list, label_list):
+    # Create and compile model
+    model = Sequential()
+    model.add(Conv2D(10, kernel_size=3, activation='relu', input_shape=(48,48, 1)), padding='valid')
+    model.add(Flatten())
+    model.add(Dense(5, activation='softmax'))
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-def train_model():
+    # Fit model to images
+    training = model.fit(img_list, label_list, validation_split=0.2, epochs=3)
 
+    # Validate model
+    plt.plot(training.history['loss'])
+    plt.plot(training.history['val_loss'])
+    plt.show()
 
-# Create and compile model
-model = Sequential()
-model.add(Conv2D(10, kernel_size=3, activation='relu', input_shape=(48,48, 1)), padding='valid')
-model.add(Flatten())
-model.add(Dense(5, activation='softmax'))
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    print('Saving model...')
+    # Save model
+    with open('cnn_model.pkl', 'wb') as output:
+        pickle.dump(model, output, pickle.HIGHEST_PROTOCOL)
+    print('Model saved in directory')
 
-# Fit model to images
-training = model.fit(train_data, train_labels, validation_split=0.2, epochs=3)
-
-# Validate model
-plt.plot(training.history['loss'])
-plt.plot(training.history['val_loss'])
-plt.show()
-
-# Save model
+if __name__ == '__main__':
+    imgs = import_images('training_images')
+    labels = import_labels('training_labels.csv')
+    train_model(imgs, labels)
