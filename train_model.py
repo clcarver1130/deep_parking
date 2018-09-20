948e5854c785839d5cc71814c34c72dcb1c4a7db
clcarver1130 <carver.cameron@gmail.com>
2018-09-20 13:59:50 -0500

    changed shape of input layer

--- train_model.py
+++ train_model.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 import pickle
 import keras
 from keras.preprocessing.image import img_to_array, load_img
@@ -16,8 +17,8 @@ def import_images():
     filelist = os.listdir('training_images/')
     train_list = []
     for file in filelist:
-        train_list.append((img_to_array(load_img('training_images/{}'.format(file), target_size=(48,48))).reshape(-1, 48,48, 1))/255)
-    return train_list
+        train_list.append(img_to_array(load_img('training_images/{}'.format(file), target_size=(48,48))))
+    return np.asarray(train_list)
 
 print('Importing labels...')
 def import_labels(CSV):
@@ -30,18 +31,13 @@ print('Fitting model...')
 def train_model(img_list, label_list):
     # Create and compile model
     model = Sequential()
-    model.add(Conv2D(10, kernel_size=3, activation='relu', input_shape=(48,48, 1), padding='same'))
+    model.add(Conv2D(10, kernel_size=3, activation='relu', input_shape=(48,48,1), padding='same'))
     model.add(Flatten())
     model.add(Dense(5, activation='softmax'))
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     # Fit model to images
-    training = model.fit(img_list, label_list, validation_split=0.2, epochs=3)
-
-    # Validate model
-    plt.plot(training.history['loss'])
-    plt.plot(training.history['val_loss'])
-    plt.show()
+    model.fit(img_list, label_list, validation_split=0.2, epochs=3)
 
     print('Saving model...')
     # Save model
