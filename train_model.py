28deb0ee34cb03247307b5b7fa86cb831f410c3f
clcarver1130 <carver.cameron@gmail.com>
2018-09-20 12:30:08 -0500

    implented data processing function

--- train_model.py
+++ train_model.py
@@ -1,4 +1,8 @@
+import os
+import pandas as pd
 import keras
+from keras.preprocessing.image import img_to_array, load_img
+from keras.utils import to_categorical
 from keras.models import Sequential,Input,Model
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -6,36 +10,37 @@ from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
 import matplotlib.pyplot as plt
 
-# Import images
+def import_images():
+    filelist = os.listdir('training_images/')
+    train_list = []
+    for file in filelist:
+        train_list.append(img_to_array(load_img('training_images/{}'.format(file), target_size=(48,48))))
+    return train_list
 
-# Preprocess images
-train_data =
-train_X = train_X.reshape(-1, 28,28, 1)
-train_X = train_X.astype('float32')
-train_X = train_X / 255.
+def import_labels():
+    df = pd.read_csv('training_labels.csv')
+    labels = df['Label']
+    labels_cat = to_categorical(labels)
+    return labels_cat
 
-train_labels =
-train_Y_one_hot = to_categorical(train_Y)
+def scale_images():
+    train_imgs_scaled = []
+    for img in train_list:
+        train_imgs_scaled.append(img/255)
+    return train_imgs_scaled
+
+def train_model():
+
 
 # Create and compile model
-batch_size = 64
-epochs = 20
-num_classes = 10
-
 model = Sequential()
-model.add(Conv2D(10, kernel_size=3, activation='relu', input_shape=(img_rows, img_cols, 1)), padding='valid')
-# # Second convolutional layer
-# model.add(Conv2D(10, kernel_size=2, activation='relu')
+model.add(Conv2D(10, kernel_size=3, activation='relu', input_shape=(48,48, 1)), padding='valid')
 model.add(Flatten())
-model.add(Dense(3, activation='softmax'))
-
-
+model.add(Dense(5, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Fit model to images
 training = model.fit(train_data, train_labels, validation_split=0.2, epochs=3)
-
-model.evaluate(test_data, test_labels, epochs=3)
 
 # Validate model
 plt.plot(training.history['loss'])
